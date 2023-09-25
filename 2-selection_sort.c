7ccbaba8551f5edc28d404994c5b66e5f3b83305
KelvinoKing <kelvinoking@gmail.com>
2023-09-25 09:38:58 +0000

    Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/*
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: pointer to array to be sorted
+ * @size: size of array to be sorted
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, hold_temp, swap = 0;
+
+	if (array == NULL)
+		return;
+	if (size <= 0 || size == 1)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		temp = array[i];
+		swap = 0;
+
+		for (j = i; j < size; j++)
+		{
+			if (temp > array[j])
+			{
+				hold_temp = temp;
+				temp = array[j];
+				array[j] = hold_temp;
+				swap = 1;
+			}
+		}
+		array[i] = temp;
+		if (swap != 0)
+			print_array(array, size);
+	}
+}
