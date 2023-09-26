29f3f72a7705dbb0a3aac8b982c0515597d63cc1
fardosa001 <fardosaadow@gmail.com>
2023-09-26 01:25:34 -0700

    fix betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *prev_node;
-	
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
@@ -16,20 +16,20 @@ void insertion_sort_list(listint_t **list)
 	while (curr != NULL)
 	{
 		prev_node = curr->prev;
-		
-		while(prev_node != NULL && prev_node->n > curr->n)
+
+		while (prev_node != NULL && prev_node->n > curr->n)
 		{
 			if (prev_node->prev != NULL)
 				prev_node->prev->next = curr;
-			
+
 			if (curr->next != NULL)
 				curr->next->prev = prev_node;
-			
+
 			prev_node->next = curr->next;
 			curr->next = prev_node;
 			curr->prev = prev_node->prev;
 			prev_node->prev = curr;
-		
+
 			print_list(*list);
 
 			prev_node = curr->prev;
